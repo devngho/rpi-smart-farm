635748b2910eeef7c4e6408c8db5d23678ca5965
devngho <yjh135908@gmail.com>
2025-10-31 21:09:48 +0900

    b

--- main.py
+++ main.py
@@ -146,4 +146,4 @@ async def websocket_live_sensor(websocket):
         await websocket.close()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, port=8000)
